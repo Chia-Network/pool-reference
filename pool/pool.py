7d1eda12f64cd24781ce81ae5076286a950906cf
William Grzybowski <william@grzy.org>
2021-07-02 00:01:09 -0300

    Fix logging for farmer points when confirming partial (#121)
    * Fix logging for farmer points when confirming partial
    
    * Log only if pool member

--- pool/pool.py
+++ pool/pool.py
@@ -539,8 +539,7 @@ class Pool:
 
                 if farmer_record.is_pool_member:
                     await self.store.add_partial(partial.payload.launcher_id, uint64(int(time.time())), points_received)
-
-                self.log.info(f"Farmer {farmer_record.launcher_id} updated points to: " f"{farmer_record.points}")
+                    self.log.info(f"Farmer {farmer_record.launcher_id} updated points to: " f"{farmer_record.points + points_received}")
         except Exception as e:
             error_stack = traceback.format_exc()
             self.log.error(f"Exception in confirming partial: {e} {error_stack}")

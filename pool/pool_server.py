fdbfea6495a427e4a3462d382abbdde9863a73aa
Michał Nowacki <michal.nowacki@reef.pl>
2021-07-06 11:56:49 +0200

    missing pass-through for difficulty function

--- pool/pool_server.py
+++ pool/pool_server.py
@@ -28,6 +28,7 @@ from chia.util.ints import uint8, uint64, uint32
 from chia.util.default_root import DEFAULT_ROOT_PATH
 from chia.util.config import load_config
 
+from .difficulty_adjustment import get_new_difficulty
 from .record import FarmerRecord
 from .pool import Pool
 from .store.abstract import AbstractPoolStore
@@ -49,10 +50,11 @@ def check_authentication_token(launcher_id: bytes32, token: uint64, timeout: uin
 
 
 class PoolServer:
-    def __init__(self, config: Dict, constants: ConsensusConstants, pool_store: Optional[AbstractPoolStore] = None):
+    def __init__(self, config: Dict, constants: ConsensusConstants, pool_store: Optional[AbstractPoolStore] = None,
+                 difficulty_function: Callable = get_new_difficulty):
 
         self.log = logging.getLogger(__name__)
-        self.pool = Pool(config, constants, pool_store)
+        self.pool = Pool(config, constants, pool_store, difficulty_function)
 
     async def start(self):
         await self.pool.start()

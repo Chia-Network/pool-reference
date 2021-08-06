ddf24b31bfbff2855d122d2c22f93d380613763e
GrassR00t <grassr00t@yahoo.com>
2021-08-06 07:43:32 +0000

    fix coin solution
    Signed-off-by: GrassR00t <grassr00t@yahoo.com>

--- pool/store/pg_store.py
+++ pool/store/pg_store.py
@@ -4,7 +4,7 @@ from typing import Optional, Set, List, Tuple, Dict
 from blspy import G1Element
 from chia.pools.pool_wallet_info import PoolState
 from chia.types.blockchain_format.sized_bytes import bytes32
-from chia.types.coin_solution import CoinSolution
+from chia.types.coin_spend import CoinSpend
 from chia.util.ints import uint64
 
 from .abstract import AbstractPoolStore
@@ -99,7 +99,7 @@ class PGStore(AbstractPoolStore):
             row[2],
             bytes.fromhex(row[3]),
             G1Element.from_bytes(bytes.fromhex(row[4])),
-            CoinSolution.from_bytes(row[5]),
+            CoinSpend.from_bytes(row[5]),
             PoolState.from_bytes(row[6]),
             row[7],
             row[8],
@@ -143,7 +143,7 @@ class PGStore(AbstractPoolStore):
     async def update_singleton(
             self,
             launcher_id: bytes32,
-            singleton_tip: CoinSolution,
+            singleton_tip: CoinSpend,
             singleton_tip_state: PoolState,
             is_pool_member: bool,
     ):

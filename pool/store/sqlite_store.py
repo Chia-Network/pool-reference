f46512bab55ebdd2835fcdeb7b747ece8bc2bfd8
Tyler <688344@qq.com>
2021-08-13 18:24:32 +0800

    Bug Fix: Inaccurate points update！
    farmer request pool "post_farmer", "put_farmer" RPC interface, causing points to be replace by historical data.

--- pool/store/sqlite_store.py
+++ pool/store/sqlite_store.py
@@ -71,22 +71,55 @@ class SqlitePoolStore(AbstractPoolStore):
         )
 
     async def add_farmer_record(self, farmer_record: FarmerRecord, metadata: RequestMetadata):
+        # Find the launcher_id exists.
         cursor = await self.connection.execute(
-            f"INSERT OR REPLACE INTO farmer VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-            (
-                farmer_record.launcher_id.hex(),
-                farmer_record.p2_singleton_puzzle_hash.hex(),
-                farmer_record.delay_time,
-                farmer_record.delay_puzzle_hash.hex(),
-                bytes(farmer_record.authentication_public_key).hex(),
-                bytes(farmer_record.singleton_tip),
-                bytes(farmer_record.singleton_tip_state),
-                farmer_record.points,
-                farmer_record.difficulty,
-                farmer_record.payout_instructions,
-                int(farmer_record.is_pool_member),
-            ),
+            "SELECT * from farmer where launcher_id=?",
+            (farmer_record.launcher_id.hex(),),
         )
+        row = await cursor.fetchone()
+        # Insert for None
+        if row is None:
+            cursor = await self.connection.execute(
+                f"INSERT INTO farmer VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                (
+                    farmer_record.launcher_id.hex(),
+                    farmer_record.p2_singleton_puzzle_hash.hex(),
+                    farmer_record.delay_time,
+                    farmer_record.delay_puzzle_hash.hex(),
+                    bytes(farmer_record.authentication_public_key).hex(),
+                    bytes(farmer_record.singleton_tip),
+                    bytes(farmer_record.singleton_tip_state),
+                    farmer_record.points,
+                    farmer_record.difficulty,
+                    farmer_record.payout_instructions,
+                    int(farmer_record.is_pool_member),
+                ),
+            )
+        # update for Exist
+        else:
+            cursor = await self.connection.execute(
+                f"UPDATE farmer SET "
+                f"p2_singleton_puzzle_hash=?, "
+                f"delay_time=?, "
+                f"delay_puzzle_hash=?, "
+                f"authentication_public_key=?, "
+                f"singleton_tip=?, "
+                f"singleton_tip_state=?, "
+                f"payout_instructions=?, "
+                f"is_pool_member=? "
+                f"WHERE launcher_id=?",
+                (
+                    farmer_record.p2_singleton_puzzle_hash.hex(),
+                    farmer_record.delay_time,
+                    farmer_record.delay_puzzle_hash.hex(),
+                    bytes(farmer_record.authentication_public_key).hex(),
+                    bytes(farmer_record.singleton_tip),
+                    bytes(farmer_record.singleton_tip_state),
+                    farmer_record.payout_instructions,
+                    int(farmer_record.is_pool_member),
+                    farmer_record.launcher_id.hex(),
+                ),
+            )
         await cursor.close()
         await self.connection.commit()
 

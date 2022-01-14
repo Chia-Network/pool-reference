19003b986119fc0ad9fb4fce6c267daf9d04b198
Jack Nelson <jack@jacknelson.xyz>
2022-01-14 17:43:49 -0500

    add wallet detection for ease of use

--- pool/pool.py
+++ pool/pool.py
@@ -564,6 +564,26 @@ class Pool:
         except Exception as e:
             error_stack = traceback.format_exc()
             self.log.error(f"Exception in confirming partial: {e} {error_stack}")
+            
+    async def validate_payout_instructions(self, payout_instructions: str) -> List[bool,Optional[bytes32]]:
+        """
+        Returns the puzzle hash from the payout instructions (puzzle hash hex or bech32m address) if it's encoded
+        correctly, otherwise returns None.
+        """
+        try:
+            if len(decode_puzzle_hash(payout_instructions)) == 32:
+                return [True, decode_puzzle_hash(payout_instructions)]
+        except Exception:
+            # Not a Chia address
+            pass
+        try:
+            if len(hexstr_to_bytes(payout_instructions)) == 32:
+                return [True, hexstr_to_bytes(payout_instructions)]
+        except Exception:
+            # Not a puzzle hash
+            pass
+        return [False, None]
+
 
     async def add_farmer(self, request: PostFarmerRequest, metadata: RequestMetadata) -> Dict:
         async with self.store.lock:
@@ -596,7 +616,7 @@ class Pool:
             if len(hexstr_to_bytes(request.payload.payout_instructions)) != 32:
                 return error_dict(
                     PoolErrorCode.INVALID_PAYOUT_INSTRUCTIONS,
-                    f"Payout instructions must be an xch address for this pool.",
+                    f"Payout instructions must be an xch address or puzzle hash for this pool.",
                 )
 
             if not AugSchemeMPL.verify(last_state.owner_pubkey, request.payload.get_hash(), request.signature):
@@ -668,14 +688,10 @@ class Pool:
                 farmer_dict["authentication_public_key"] = request.payload.authentication_public_key
 
         if request.payload.payout_instructions is not None:
-            is_new_value = (
-                farmer_record.payout_instructions != request.payload.payout_instructions
-                and request.payload.payout_instructions is not None
-                and len(hexstr_to_bytes(request.payload.payout_instructions)) == 32
-            )
-            response_dict["payout_instructions"] = is_new_value
-            if is_new_value:
-                farmer_dict["payout_instructions"] = request.payload.payout_instructions
+            new_ph = self.validate_payout_instructions(request.payload.payout_instructions)
+            response_dict["payout_instructions"] = new_ph[0]
+            if new_ph[0]:
+                farmer_dict["payout_instructions"] = new_ph[1]
 
         if request.payload.suggested_difficulty is not None:
             is_new_value = (
